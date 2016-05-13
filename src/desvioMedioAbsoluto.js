4d6bb60e112bc957fbff0ca7f22a1444e3d2ab39
William Dias <williamdiasvargas@gmail.com>
2016-05-13 15:29:17 -0400

    atuliazacao do desvioMedioAbsoluto
    atuliazacao do desvioMedioAbsoluto refatoracao

--- src/desvioMedioAbsoluto.js
+++ src/desvioMedioAbsoluto.js
@@ -1,13 +1,17 @@
 'use strict';
- /**/
-var tamanho = require('./tamanho');
+ /*
+ *var arr=[1,2,3]
+ *media = 2
+ *tamanho = 3
+ *somatorio = 6
+ *desvioMedioAbsoluto resultado esperado = 0.6667
+ */
+'use strict';
 var media = require('./mediaAritmetica');
-var somatorio = require('./somatorio');
+var tamanho = require('./tamanho');
 
-// ainda nao resolvi
-function desvioMedioAbsoluto (arr,somatorio,media,tamanho){
-
-	return somatorio(arr[index]- media(arr))/tamanho(arr);
-
+function desvioMedioAbsoluto () {
+  return arr.map(function(elemen){ return elemen - media;}).reduce(function (a,b){return a + b})/tamanho;
 }
 module.exports = desvioMedioAbsoluto;
+
