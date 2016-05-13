32a18a9b9786d122a072681ce17963f86377b480
William Dias <williamdiasvargas@gmail.com>
2016-05-12 20:13:20 -0400

    update functions_PE.js

--- functions_PE.js
+++ functions_PE.js
@@ -1,6 +1,28 @@
 /*MICRO_LIBRARY_PROBABILTY_AND_STATISTIC*/
 /*AUTHOR: WILLIAM_DIAS_VARGAS*/
+
+
+
 /*VERSION:0.01*/
+//QUARTIZ,PERCENTIZ,DECIS
+//Agrupados por intervalo de Classe
+
+function separatriz(li,i,N,D,Fant,fmd,h){
+	var sep = (li+((i*(N/D)-Fant)/fmd)*h);
+	return sep.toFixed(3);
+}
+function mode(linf,d1,d2,h) {
+	return linf+((d1/(d1+d2))*h);
+}
+function median(linf,N,Fant,fmd,h) {
+	return linf+((N/2)-Fant)/fmd*h;
+}
+function mean(xifi,fi) {
+	return xifi/fi;
+}
+function curtose(q3,q1,p90,p10) {
+	return ((q3-q1)/(2*(p90-p10)));
+}
 
 
 
